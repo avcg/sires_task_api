6ff6cf5d0b12ca09cf5d5c6b86dbbab8520bbb53
Malut <rusakov.paul@gmail.com>
2019-04-08 19:18:02 +0300

    Add options for CORS to all endpoints

--- lib/sires_task_api_web/router.ex
+++ lib/sires_task_api_web/router.ex
@@ -16,6 +16,7 @@ defmodule SiresTaskApiWeb.Router do
     pipe_through :api
 
     resources "/users", UserController, only: [:create]
+    options "/users", UserController, :options
 
     post "/sign_in", SignInController, :sign_in
     options "/sign_in", SignInController, :options
@@ -27,31 +28,51 @@ defmodule SiresTaskApiWeb.Router do
     pipe_through [:api, :protected]
 
     get "/current_user", CurrentUserController, :show
+    options "/current_user", CurrentUserController, :options
+
     get "/tasks/calendar", TaskController, :calendar, as: :calendar
+    options "/tasks/calendar", TaskController, :options
 
     resources "/users", UserController, only: [:index, :show, :update] do
+
       post "/deactivate", UserController, :deactivate, as: :deactivate
+      options "/deactivate", UserController, :options
+
       post "/activate", UserController, :activate, as: :activate
+      options "/activate", UserController, :options
+
     end
 
     resources "/projects", ProjectController, only: [:index, :show, :create, :update, :delete] do
       resources "/members", Project.MemberController, only: [:create, :update, :delete]
+      options "/members", Project.MemberController, :options
     end
 
     resources "/tasks", TaskController, only: [:index, :show, :create, :update, :delete] do
       post "/mark_undone", TaskController, :mark_undone, as: :mark_undone
+      options "/mark_undone", TaskController, :options
+
       post "/mark_done", TaskController, :mark_done, as: :mark_done
+      options "/mark_done", TaskController, :options
 
       resources "/members", Task.MemberController, only: [:create, :delete]
+      options "/members", Task.MemberController, :options
+
+
       resources "/references", Task.ReferenceController, only: [:create, :delete]
+      options "/references", Task.ReferenceController, :options
+
       resources "/comments", Task.CommentController, only: [:create, :update, :delete]
+      options "/comments", Task.CommentController, :options
 
       resources "/attachments", Task.AttachmentController, only: [] do
         resources "/versions", Task.Attachment.VersionController, only: [:index, :create]
+        options "/versions", Task.Attachment.VersionController, :options
       end
     end
 
     resources "/tags", TagController, only: [:index, :create, :update, :delete]
+    options "/tags", TagController, :options
   end
 
   # Swagger (API live documentation)
