3ef77ff26205d88bde12b1d6ea2adc5f8673a7e1
Malut <rusakov.paul@gmail.com>
2019-09-13 12:07:10 +0300

    Change edeliver config 8

--- rel/config.exs
+++ rel/config.exs
@@ -7,7 +7,7 @@
 |> Path.wildcard()
 |> Enum.map(&Code.eval_file(&1))
 
-use Mix.Releases.Config,
+use Distillery.Releases.Config,
     # This sets the default release built by `mix release`
     default_release: :default,
     # This sets the default environment used by `mix release`
@@ -52,4 +52,3 @@ release :sires_task_api do
     :runtime_tools
   ]
 end
-
