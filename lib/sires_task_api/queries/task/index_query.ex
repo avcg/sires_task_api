69b3a36e360e45f84cce9b05a9fd5ac507f064e7
Timofey Martynov <feymartynov@gmail.com>
2019-10-01 14:02:30 +0300

    Fix join alias

--- lib/sires_task_api/queries/task/index_query.ex
+++ lib/sires_task_api/queries/task/index_query.ex
@@ -12,7 +12,7 @@ defmodule SiresTaskApi.Task.IndexQuery do
   def build_query(%User{id: user_id} = user, dynamic, opts) do
     Task
     |> Bodyguard.scope(user)
-    |> join(:left, [t], tm in assoc(t, :members), as: :tag_members)
+    |> join(:left, [t], tm in assoc(t, :members), as: :task_members)
     |> join(:left, [t], tg in assoc(t, :tags), as: :tags)
     |> where([tag_members: tm], is_nil(tm.user_id) or tm.user_id == ^user_id)
     |> where(^dynamic)
