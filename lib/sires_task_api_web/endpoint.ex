7fa9253f8750503323191195c2468fbbf267d962
Timofey Martynov <feymartynov@gmail.com>
2019-10-10 04:17:39 +0300

    Increase max upload size to 88 Mb

--- lib/sires_task_api_web/endpoint.ex
+++ lib/sires_task_api_web/endpoint.ex
@@ -31,7 +31,8 @@ defmodule SiresTaskApiWeb.Endpoint do
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
-    json_decoder: Phoenix.json_library()
+    json_decoder: Phoenix.json_library(),
+    length: 88_000_000
 
   plug Plug.MethodOverride
   plug Plug.Head
